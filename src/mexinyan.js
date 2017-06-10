7efd90a0938d923a754f68b92bfa3d6d68ba27e4
ArtemisD <dianah456@gmail.com>
2017-06-10 18:13:08 -0400

    pass bare minimum requirements

---
+++ src/mexinyan.js
@@ -0,0 +1,105 @@
+var mexinyan = function(top, left, timeBetweenSteps) {
+  MakeDancer.call(this, top, left, timeBetweenSteps);
+  this.name = 'mexinyan';
+  this.rainbowWrap = $(
+    '<div class="rainbow_wrap">\
+    </div>\
+  ');
+  this.rainbowOne = $(
+    '<div class="rainbow_one">\
+      <div class="rainbow_top"></div>\
+      <div class="rainbow_second_top"></div>\
+      <div class="rainbow_middle_top"></div>\
+      <div class="rainbow_second_bottom"></div>\
+      <div class="rainbow_bottom"></div>\
+    </div>\
+  ');
+  this.rainbowTwo = $(
+    '<div class="rainbow_two">\
+      <div class="rainbow_top"></div>\
+      <div class="rainbow_second_top"></div>\
+      <div class="rainbow_middle_top"></div>\
+      <div class="rainbow_second_bottom"></div>\
+      <div class="rainbow_bottom"></div>\
+    </div>\
+  ');
+  this.rainbowThree = $(
+    '<div class="rainbow_three">\
+      <div class="rainbow_top"></div>\
+      <div class="rainbow_second_top"></div>\
+      <div class="rainbow_middle_top"></div>\
+      <div class="rainbow_second_bottom"></div>\
+      <div class="rainbow_bottom"></div>\
+    </div>\
+  ');
+  this.rainbowFour = $(
+    '<div class="rainbow_four">\
+      <div class="rainbow_top"></div>\
+      <div class="rainbow_second_top"></div>\
+      <div class="rainbow_middle_top"></div>\
+      <div class="rainbow_second_bottom"></div>\
+      <div class="rainbow_bottom"></div>\
+    </div>\
+  ');
+  this.rainbowFive = $(
+    '<div class="rainbow_five">\
+      <div class="rainbow_top"></div>\
+      <div class="rainbow_second_top"></div>\
+      <div class="rainbow_middle_top"></div>\
+      <div class="rainbow_second_bottom"></div>\
+      <div class="rainbow_bottom"></div>\
+    </div>\
+  ');
+  this.nyanGif = $('<img class="nyan" src="src/mexinyan.gif"></img>');
+  $(this.rainbowWrap).append(this.rainbowOne);
+  $(this.rainbowWrap).append(this.rainbowTwo);
+  $(this.rainbowWrap).append(this.rainbowThree);
+  $(this.rainbowWrap).append(this.rainbowFour);
+  $(this.rainbowWrap).append(this.rainbowFive);
+};
+
+mexinyan.prototype = Object.create(MakeDancer.prototype);
+mexinyan.prototype.constructor = mexinyan;
+
+mexinyan.prototype.step = function(timeBetweenSteps) {
+  MakeDancer.prototype.step.call(this);
+  $(this.rainbowTwo).animate({
+    top: "-=10",
+  }, 100);
+  $(this.rainbowThree).animate({
+    top: "-=20",
+  }, 100);
+  $(this.rainbowFour).animate({
+    top: "-=10",
+  }, 100);
+
+  $(this.rainbowTwo).animate({
+    top: "+=10",
+  }, 100);
+  $(this.rainbowThree).animate({
+    top: "+=20",
+  }, 100);
+  $(this.rainbowFour).animate({
+    top: "+=10",
+  }, 100);
+
+  $(this.rainbowTwo).animate({
+    top: "+=10",
+  }, 100);
+  $(this.rainbowThree).animate({
+    top: "+=20",
+  }, 100);
+  $(this.rainbowFour).animate({
+    top: "+=10",
+  }, 100);
+
+  $(this.rainbowTwo).animate({
+    top: "-=10",
+  }, 100);
+  $(this.rainbowThree).animate({
+    top: "-=20",
+  }, 100);
+  $(this.rainbowFour).animate({
+    top: "-=10",
+  }, 100);
+};
---
+++ src/pikanyan.js
@@ -0,0 +1,14 @@
+var pikanyan = function(top, left, timeBetweenSteps) {
+  mexinyan.call(this, top, left, timeBetweenSteps);
+  this.name = "pikanyan";
+  this.nyanGif = $('<img class="nyan" src="src/pikanyan.gif"></img>');
+  var filterVal = 'invert(100%)';
+  $(this.rainbowWrap).css("filter", filterVal);
+};
+
+pikanyan.prototype = Object.create(mexinyan.prototype);
+pikanyan.prototype.constructor = pikanyan;
+
+pikanyan.prototype.step = function(timeBetweenSteps) {
+  mexinyan.prototype.step.call(this);
+};
